5ef09f176acbc60569bf4a841382db75832db0d3
Siddhesh Rajendra Gadhave <siddhesh.gadhave.aids.2020@vpkbiet.org>
2023-02-10 00:47:10 +0530

    Add files via upload

---
+++ QuestionAnswer.java
@@ -0,0 +1,26 @@
+package com.example.myquizapplication;
+
+public class QuestionAnswer {
+
+    public static String question[] ={
+            "Name the national bird of India?",
+            "Which one is not the programming language?",
+            "Where you are watching this video?",
+            "Which company owns the Apple?"
+    };
+
+    public static String choices[][] = {
+            {"Peacock","Crow","sparrow","parrots"},
+            {"Java","Kotlin","Notepad","Python"},
+            {"Facebook","Whatsapp","Instagram","Youtube"},
+            {"Google","Apple","Nokia","Samsung"}
+    };
+
+    public static String correctAnswers[] = {
+            "Peacock",
+            "Notepad",
+            "Youtube",
+            "Apple"
+    };
+
+}
